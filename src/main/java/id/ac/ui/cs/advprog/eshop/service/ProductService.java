2db83d77e2bac9f43f5abd0a398bd09877c10f3d
Vincentius Filbert Amadeo <vincentius.filbert@ui.ac.id>
2026-03-03 16:22:40 +0700

    Add CarController and update product delete flow

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -1,12 +1,14 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
+
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product findById(String productId);
-    public void update(String productId, Product product);
-    public void delete(String productId);
+    Product create(Product product);
+    List<Product> findAll();
+    Product findById(String productId);
+    void update(String productId, Product product);
+    void delete(String productId);
+    void deleteProductById(String productId);
 }
